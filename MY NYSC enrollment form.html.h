2f902cbb7a51099724fc43f43f5a33bcf740e582
Godsheritage425 <150611198+Godsheritage425@users.noreply.github.com>
2023-11-14 22:47:11 +0100

    Add files via upload

---
+++ MY NYSC enrollment form.html.h
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+       <title >
+           <h1>MY First Nysc Tech Assignment</h1>  
+           </title>
+          <style type="text/css">
+          Body{
+              Background-color:lightblue;
+              Font-family:Serif;
+              Font-size:10px;
+              Text-align:left;
+          }
+          h1, h2, h3{
+              Color:white;
+              Border:2px;
+              border-style:ridge;
+              Border-color: black;
+              }
+              Label{
+                  Color:red;
+              }
+              
+              </style>
+     </head>
+     <body>
+<h2>
+    Different between checkbox and radio 
+</h2> 
+    <p>
+        Checkboxs are use to have multiple selection, example;</p>
+        <label>courses you will like to take:</label>
+      Data science<input type ="checkbox" name ="courses ">
+      Product management<input type ="checkbox" name="course">
+      Frontend Engeneering<input type="checkbox" name ="course"><br><br>
+ <p>Radio are use for just one selection, example;</p>
+ <label>Choose gender</label>
+ <input type="radio" name="gender">Male
+ <input type ="radio" name="gender">female
+       
+         <h2>Provide the necessary information </h2>
+        <form>
+          <label class="mylabel">Name</label> 
+          <input type ="text" placeholder="enter surname first" class ="form control"> i<br><br>
+          <label>Email</label>
+          <input type="email" placeholder="not capital letter"><br><br>
+          <label>Username</label>
+          <input type ="text"><br><br>
+          <label>Nationality</label>
+          <select>
+              <option>Ivory Coast </option>
+              <option>Ghana</option>
+              <option>Nigeria</option>
+              <option>USA</option>
+              <option>United Kingdom</option>
+          </select><br><br>
+          <label>Business name </label>
+          <input type="text"><br><br>
+          <label>Phone number </label>
+          <input type="number"><br><br>
+          <label>Gender</label>
+          <input type ="radio" name="gender"> male
+          <input type ="radio" name="gender"> female
+          <input type ="radio" name="gender"> By sexual <br><br>
+          <label>Choose the category you want to register your business </label>
+          <select>
+              <option>Business name registration </option>
+              <option>Company Name Registration </option>
+              <option>incorporated trustees </option>
+          </select><br><br>
+          <input type ="submit" value="submit form">
+          <input type ="Reset"  value ="reset">
+          <a href="tel:07089834952">call now</a>
+     </form> 
+         <p> P is use for creating a paragraph </p>
+         <h4> This is headline 4. It means the forth headline. We have From h1 to h6</h4>
+         <p> Marquee allow text to move from either left to right or right to left</p>
+         <div class="container">
+             
+         </div>
+     </body>
+     KW/23A/0800 
